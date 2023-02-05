a1c81bdb7ff97be82de1d99e9a1f8de9a57e5c07
jithin C <jithincotp@gmail.com>
2023-02-05 15:10:52 +0530

    Modified parsing groovy file

--- vars/parsing.groovy
+++ vars/parsing.groovy
@@ -1,19 +1,24 @@
-def xml = '''
-<AS Name="123">
-    <Config Name="Configuration1">
-        <Servers>
-            <Server Name="server1"/>
-            <Server Name="server2"/>
-            <Server Name="server3"/>
-            <Server Name="server4"/>
-         </Servers>
-    </Config>
-</AS>
-'''
+def call(def userarg1)
+{
+    println "Hello $userarg1"
+    //
+    def xml = '''
+    <AS Name="123">
+        <Config Name="Configuration1">
+            <Servers>
+                <Server Name="server1"/>
+                <Server Name="server2"/>
+                <Server Name="server3"/>
+                <Server Name="server4"/>
+            </Servers>
+        </Config>
+    </AS>
+    '''
 
 def config = new XmlParser().parseText(xml)
 
 config.Config.Servers.Server.each{
     println it.@Name
     //println "${env.WORKSPACE}/parameters.xml"
+    }
 }
