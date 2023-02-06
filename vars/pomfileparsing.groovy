551e716a17a3144231d93e3e85efb3f69254bcbd
jithin C <jithincotp@gmail.com>
2023-02-06 19:55:32 +0530

    First groovy modification

---
+++ vars/pomfileparsing.groovy
@@ -0,0 +1,12 @@
+def call(def userarg , def responsedata)
+{ 
+    def xml = responsedata
+    def config = new XmlParser().parseText(xml)
+    project.dependencyManagement.dependencies.dependency.each{
+        println it.@groupId
+    }
+    // config.Config.Servers.Server.each{
+        // println it.@Name
+    //println "${env.WORKSPACE}/parameters.xml"
+    }
+}
