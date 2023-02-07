6ae5a96657c2f27966d22299aba78704f0c97d42
jithin C <jithincotp@gmail.com>
2023-02-07 19:37:33 +0530

    project tag issue fixed

--- vars/pomfileparsing.groovy
+++ vars/pomfileparsing.groovy
@@ -1,11 +1,16 @@
+import groovy.xml.*
 def call(def responsedata)
 {   
     println " = = = = Parsing Initialized = = = = "
+    // assigning argument variable value to xml variable
     def xml = responsedata
-    xml = xml.replace("""<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">""","")
-    xml = xml.replace("</project>","")
-    string config = new XmlParser().parseText(xml)
+
+    // replacing and removing the namespace things from xml
+    xml = xml.replace("""xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd" ""","")
+    
+    // Using parser function parsing the xml
+    def config = new XmlParser().parseText(xml)
     // def dependencies = config.dependencies.dependency.collect { dep ->
     //     dep.groupId.text()
   	// }
