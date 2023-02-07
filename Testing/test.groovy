48be99f4885743d0cbbc3087cf0b1f928cd85cda
jithin C <jithincotp@gmail.com>
2023-02-07 18:26:57 +0530

    Updated

--- Testing/test.groovy
+++ Testing/test.groovy
@@ -1,9 +1,8 @@
+import groovy.xml.*
 def call(def userarg1)
 {     
     println "Hello $userarg1"
     def xml = '''
-    <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-            xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
     <dependencyManagement>
         <dependencies>
         <dependency>
@@ -66,8 +65,13 @@ def call(def userarg1)
     </dependencyManagement>
     '''
     def config = new XmlParser().parseText(xml)
-    project.dependencyManagement.dependencies.dependency.each{
-    println it.@artifactId
-    }
+  	def dependencies = config.dependencyManagement.dependencies.dependency.collect { dep ->
+      //"${dep.groupId}:${dep.artifactId}:${dep.version}"
+      dep.groupId
+  	}
+  	println "Dependencies:"
+    dependencies.each { println " - $it" }
+    
+    //println "${config.dependencyManagement/dependencies/dependency[2]/groupId.toString()}"
   }
   call ("jithin")
--- vars/pomfileparsing.groovy
+++ vars/pomfileparsing.groovy
@@ -3,12 +3,15 @@ def call(def responsedata)
     println " = = = = Parsing Initialized = = = = "
     def xml = responsedata
     def config = new XmlParser().parseText(xml)
+    def dependencies = config.dependencies.dependency.collect { dep ->
+    dep.groupId
+  	}
+    println "Dependencies groupids:"
+    dependencies.each { println "- $it" }
 
-    def parsed = config.version.text()
-    println(parsed)
     // project.dependencyManagement.dependencies.dependency.each{
     //     println it.@groupId
-    }
+    //}
     // config.Config.Servers.Server.each{
         // println it.@Name
     //println "${env.WORKSPACE}/parameters.xml"
