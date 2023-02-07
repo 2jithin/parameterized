f6252ea71b63835c6cd0e823e94e580c5e8796c9
jithin C <jithincotp@gmail.com>
2023-02-07 19:29:25 +0530

    def changed to string

--- vars/pomfileparsing.groovy
+++ vars/pomfileparsing.groovy
@@ -5,7 +5,7 @@ def call(def responsedata)
     xml = xml.replace("""<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">""","")
     xml = xml.replace("</project>","")
-    def config = new XmlParser().parseText(xml)
+    string config = new XmlParser().parseText(xml)
     // def dependencies = config.dependencies.dependency.collect { dep ->
     //     dep.groupId.text()
   	// }
