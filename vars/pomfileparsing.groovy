94a29da2c350c0e5a2f616c106c69e604e8ed66a
jithin C <jithincotp@gmail.com>
2023-02-07 19:25:14 +0530

    Fix3

--- vars/pomfileparsing.groovy
+++ vars/pomfileparsing.groovy
@@ -5,10 +5,7 @@ def call(def responsedata)
     xml = xml.replace("""<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">""","")
     xml = xml.replace("</project>","")
-    println(xml)
     def config = new XmlParser().parseText(xml)
-    println(config)
-
     def dependencies = config.dependencies.dependency.collect { dep ->
         dep.groupId.text()
   	}
